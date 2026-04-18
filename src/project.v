75d49360919cc5a2636b82676f66ae8e8e22c872
trantai200701-eng <trantai200701@gmail.com>
2026-04-18 15:41:19 +0700

    Update info.yaml with 6 analog pins including VGND, add GDS layout

--- src/project.v
+++ src/project.v
@@ -1,14 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Engineer of CTW
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire       VGND,
-    input  wire       VDPWR,    // 1.8v power supply
-//    input  wire       VAPWR,    // 3.3v power supply
+module tt_um_ctw_ldo (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -19,5 +16,11 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+
+    // BẮT BUỘC: Gán tất cả các chân Digital Output không sử dụng xuống mức 0 (Ground)
+    // Điều này giúp hệ thống CI/CD tổng hợp mạch không bị báo lỗi Floating Net.
+    assign uo_out  = 8'b0;
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
 
 endmodule
