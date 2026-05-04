4e52e4ff9cfbda2f77940ad0125b8d38bf6f6ba9
rakuichijo2001-sketch <rakuichijo2001@gmail.com>
2026-05-04 11:57:59 +0700

    Update and rename project.v to tt_um_ctw_ldo.v

--- src/tt_um_ctw_ldo.v
+++ src/tt_um_ctw_ldo.v
@@ -1,11 +1,14 @@
 /*
- * Copyright (c) 2024 Engineer of CTW
+ * Copyright (c) 2024 Your Name
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_ctw_ldo (
+module tt_um_example (
+    input  wire       VGND,
+    input  wire       VDPWR,    // 1.8v power supply
+//    input  wire       VAPWR,    // 3.3v power supply
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,11 +19,5 @@ module tt_um_ctw_ldo (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
-    // BẮT BUỘC: Gán tất cả các chân Digital Output không sử dụng xuống mức 0 (Ground)
-    // Điều này giúp hệ thống CI/CD tổng hợp mạch không bị báo lỗi Floating Net.
-    assign uo_out  = 8'b0;
-    assign uio_out = 8'b0;
-    assign uio_oe  = 8'b0;
 
 endmodule
